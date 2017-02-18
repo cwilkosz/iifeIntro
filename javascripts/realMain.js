405465b4531eb1782aa3e904fc0d86e2bb7f9ad9
C Wilkosz <theewilkyway@gmail.com>
2017-02-18 11:19:19 -0600

    fixedtypo

---
+++ javascripts/realMain.js
@@ -0,0 +1,7 @@
+console.log("Planets ", SolarSystem.getPlanets());
+console.log("Planets with People: ", SolarSystem.getPlanetsLandedPeopleOn());
+SolarSystem.setSpaceships("Voayager 1");
+SolarSystem.setSpaceships("Voayager 2");
+SolarSystem.setSpaceships("Gemini");
+SolarSystem.setSpaceships("Apollo");
+console.log("Spaceships: ", SolarSystem.getSpaceships());
--- javascripts/solarsystem.js
+++ javascripts/solarsystem.js
@@ -1,21 +1,10 @@
 var SolarSystem = (function(){
 
-	var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
-	var numPlanetsPeopleLandedOn = 0;
+
 	
-	var stars =["Sun", "Alpha Centauri", "Wolf 359"];
-	var age = 0;
 	return {
 		setDate: Date.now(),
-		getPlantes: function(){
-			return planets;
-		},
-		getPlanetsLandedPeopleOn: function(){
-			return numPlanetsPeopleLandedOn;	
-		},
-		setPlanetsLandedPeopleOn: function(){
-			numPlanetsPeopleLandedOn++;
-		},
+		
 		
 		getDwarfPlanets : function (){
 			return "they are rejects";
@@ -23,25 +12,8 @@ var SolarSystem = (function(){
 		setDwarfPlanets : function(lumpOfRock){
 			dwarfPlanets.push(lumpOfRock);
 		},
-		getStars : function(){
-			return stars;
-		},
-		setStars : function(newStar){
-			stars.push(newStar);
-		},
-		getSolarSytemsAge : function(){
-			return age;
-		},
-		setSolarSytemsAge: function(){
-			age++;
-		}
-	}
+		
+		
+	};
 })();
 
-// console.log("Planets ", SolarSystem.getPlantes());
-// console.log("Planets with People: ", SolarSystem.getPlanetsLandedPeopleOn());
-// SolarSystem.setSpaceships("Voayager 1");
-// SolarSystem.setSpaceships("Voayager 2");
-// SolarSystem.setSpaceships("Gemini");
-// SolarSystem.setSpaceships("Apollo");
-// console.log("Spaceships: ", SolarSystem.getSpaceships());
